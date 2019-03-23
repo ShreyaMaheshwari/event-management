d2b91210e1cdc0b1e9e1a92fc9566cb6c8b22a5f
ShreyaMaheshwari <shreya.2506@gmail.com>
2019-03-23 16:08:10 +0530

    Add files via upload

---
+++ Phase-2/create.sql
@@ -0,0 +1,60 @@
+CREATE TABLE STUDENT(SName 	VARCHAR(40) 	NOT NULL,
+					 SPhone 	CHAR(10)				,
+					 SEmail 	VARCHAR (30)    NOT NULL,
+					 SStream 	VARCHAR (20)			,
+					 SId 		VARCHAR (10) 		PRIMARY KEY 
+					 );
+					 
+CREATE TABLE HOBBY( ID 			VARCHAR(10) 				,
+					Hobby 		VARCHAR(15) 	NOT NULL,
+					FOREIGN KEY(ID) REFERENCES STUDENT(SId) ON DELETE CASCADE
+				  );
+						
+						
+CREATE TABLE ORGANISER( Oname   VARCHAR(40)		NOT NULL,
+						OEmail  VARCHAR(20)		NOT NULL,
+						Ophone  CHAR(10)     NOT NULL,
+						OId     VARCHAR(10)        PRIMARY KEY
+					  );
+					   
+CREATE TABLE EVENT( EName 		VARCHAR(40)		NOT NULL,	
+					EMaxPar     INT 			NOT NULL,
+					EFee		DECIMAL(9,2)			,
+					ESize		INT				NOT NULL,
+					EType		VARCHAR(15)				,
+					EVenue		VARCHAR(20)		NOT NULL,
+					EId			VARCHAR(10) 	PRIMARY KEY 
+				  );
+				  
+CREATE TABLE EVDATE(ID 		CHAR(10) 		,
+					EDate	DATE 	NOT NULL,
+					FOREIGN KEY(ID) REFERENCES EVENT(EId) ON DELETE CASCADE
+					);
+					
+CREATE TABLE REGISTRATION( RPrize 			VARCHAR(15) DEFAULT 'NONE'	,
+						   RCertificate  	BOOLEAN 	DEFAULT 'FALSE' , 
+						   REvId			VARCHAR(10)					,
+						   RId 				VARCHAR(10)	PRIMARY KEY 
+						   FOREIGN KEY(REvId) REFERENCES EVENT(EId) ON DELETE CASCADE
+						  );
+
+CREATE TABLE TEAM_MEM(RId VARCHAR(10)	,
+					  SId VARCHAR(10)	, 
+					  FOREIGN KEY(RId) REFERENCES REGISTRATION(RId), 
+					  FOREIGN KEY(SId) REFERENCES STUDENT(SId));
+						  
+						  
+CREATE TABLE UPDATES (OId 	VARCHAR(10) , 
+					  RId VARCHAR(10) ,
+					  FOREIGN KEY(OId) REFERENCES ORGANISER(OId),
+					  FOREIGN KEY(RId) REFERENCES REGISTRATION(RId)
+					  );
+					  
+					  
+						
+CREATE TABLE CREATES(EId 	VARCHAR(10) ,
+					 OId	VARCHAR(10) , 
+					 FOREIGN KEY(EId) REFERENCES EVENT(EId), 
+					 FOREIGN KEY(OId) REFERENCES ORGANISER(OId)
+					);
+						
